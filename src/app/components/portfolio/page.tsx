c9332cd0973026d0c98790fccef1fdc4f1ea80f0
AyeshaMaryam-1 <maryamayesha681@gmail.com>
2024-10-15 17:13:04 +0500

    Add files via upload

---
+++ src/app/components/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/app/components/header";
+
+export default function Portfolio(){
+    return(
+        <div>
+            <img src="/portfolio.jpg" alt="img" className="portfolio-pic"/>
+        </div>
+    )
+}
