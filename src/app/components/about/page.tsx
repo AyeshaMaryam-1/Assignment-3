ba60cf2a5dc933bad506b25748df49dd88ecfc83
AyeshaMaryam-1 <maryamayesha681@gmail.com>
2025-01-04 21:19:57 +0500

    Update page.tsx

--- src/app/components/about/page.tsx
+++ src/app/components/about/page.tsx
@@ -7,10 +7,10 @@ export default function About() {
         <h1>AYESHA MARYAM</h1>
         <h2>(Software Developer & AI Enthusiast)</h2>
         <br />
-        <p>Hi, I'm Ayesha Maryam, a software developer and passionate web enthusiast, dedicated to crafting cutting-edge digital solutions and explore AI-driven solutions</p>
+        <p>Hi, I&apos;m Ayesha Maryam, a software developer and passionate web enthusiast, dedicated to crafting cutting-edge digital solutions and explore AI-driven solutions</p>
       </div>
       <div className="right-side">  
-        <img src="/img.jpg" alt="img"/>
+        <Image src="/img.jpg" alt="img" width={633} height={634}/>
       </div>      
     </div>
   );
